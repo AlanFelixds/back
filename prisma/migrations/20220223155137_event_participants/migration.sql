95e59d8e69ff4bc3904981bd970a91c8914bbc7e
Alan Felix <alanfelixds@gmail.com>
2022-02-23 12:53:55 -0300

    job migrate dev

---
+++ prisma/migrations/20220223155137_event_participants/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `Events` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `participantsID` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Participants` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Events` ADD CONSTRAINT `Events_participantsID_fkey` FOREIGN KEY (`participantsID`) REFERENCES `Participants`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
